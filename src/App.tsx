770c708d885b19dde3f1b940434454d5760d8aac
Olli Tanhuanpää <ottanh@utu.fi>
2022-12-17 12:46:33 +0200

    add map with hsl-map-style

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,27 @@
 import './App.css';
+import Map from 'react-map-gl';
+import mapboxgl from 'mapbox-gl';
+import style from './hsl-map-style'
+
+if(process.env.REACT_APP_MAPBOX_TOKEN) {
+  mapboxgl.accessToken = process.env.REACT_APP_MAPBOX_TOKEN;
+}
+
 
 function App() {
+
   return (
     <div className="App">
       hello
+      <Map
+        initialViewState={{
+          longitude: 24.92399127219203,
+          latitude: 60.20098737761848,
+          zoom: 9
+        }}
+        mapStyle={style}
+        attributionControl={false}
+      />
     </div>
   );
 }
